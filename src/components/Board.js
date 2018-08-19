1dd084c36b3d65a6d17dd9dda1f018e21578fdf7
codeepic <ksaweryglab@gmail.com>
2018-08-19 20:59:20 +0100

    feat: inter component communication

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,11 +2,65 @@ import React, {Component} from "react";
 import {Square} from "./Square";
 
 export class Board extends React.Component {
-    render() {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+
+        console.log('BOARDDD state', this.state);
+    }
+
+    handleClick(i){
+        // this.setState({squares: })
+        console.log('clicked quare - MSG FROM BOARD ', i);
+    }
+
+    //now we're passing 2 props from Board to Square, value and onClick
+    //The onClick prop is a function that square can call when clicked.
+    // renderSquare(i){
+    //     return <Square
+    //         value={this.state.squares[i]}
+    //         onSquareClick={() => this.handleSquareClick(i)}
+    //     />
+    // }
+
+    renderSquare(i) {
         return (
-            <div className="board">
-                <Square />
+            <Square
+                value={this.state.squares[i]} //passing null as initial value
+                // value={i}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next player: Xavier';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board">
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,45 @@ import React, {Component} from "react";
 
 
 export class Square extends React.Component {
+    constructor(props){
+        super(props);
+        console.log('square props: ', this.props)
+    }
+
     render(){
+        //onClick calls parent onClick function that was passed as a prop
         return (
-            <div className="square">
-                square
-            </div>
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
+            </button>
         );
     }
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         value: null
+    //     }
+    // }
+    //
+    // showMessage(){
+    //     console.log('I am message');
+    //     console.log('props:: ', this.props);
+    //     // console.log('thisisL ', this);
+    //     //
+    //     //
+    //     // console.log('clicked on btn ', this.props.value);
+    //     // console.log('props are::: ', this.props);
+    // }
+    //
+    // render(){
+    //     return (
+    //         <button className="square" onClick={() => this.setState({value: 'X'})}>
+    //         {/*<button className="square" onClick={() => this.showMessage()}>*/}
+    //         {/*<button className="square" onClick={this.showMessage.bind(this)}>*/}
+    //             props {this.props.value}&nbsp;
+    //             state {this.state.value}
+    //         </button>
+    //     );
+    // }
 }
